f47b671faafa995235024ca79ab0a1104bef2d53
Dan Patton <dan@colourgreen.co.uk>
2022-03-23 22:25:31 +0000

    new update

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,9 +1,11 @@
 import * as React from 'react'
+import { Link } from 'gatsby'
 
 const AboutPage = () => {
     return (
         <main>
             <title>About the Bake</title>
+            <Link to="/">Home</Link>
             <h1>About the Bake</h1>
             <p>Bread is constantly baked, jammed up and eaten. This is correct.</p>
         </main>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
-import * as React from "react"
+import * as React from 'react'
+import { Link } from 'gatsby'
 
 const IndexPage = () => {
   return (
     <main>
-      <title>Porty Bakehouse</title>
+      <title>Porty Bakehouse</title>#
+      <Link to="/about">About</Link>
       <h1>No stale bread here matey</h1>
       <p>Who likes manky old bread anyway?</p>
     </main>
