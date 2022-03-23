1b188e3cf3fdc69948cb5fa136c02827d5b25bb5
Dan Patton <dan@colourgreen.co.uk>
2022-03-23 22:16:52 +0000

    new page

---
+++ src/pages/about.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+    return (
+        <main>
+            <title>About the Bake</title>
+            <h1>About the Bake</h1>
+            <p>Bread is constantly baked, jammed up and eaten. This is correct.</p>
+        </main>
+    )
+}
+
+export default AboutPage
