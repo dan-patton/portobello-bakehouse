4ff36ef0d76dfc696efd12861c2f7023f49b9ab0
Dan Patton <dan@colourgreen.co.uk>
2022-03-28 21:27:52 +0100

    other pic

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -6,6 +6,7 @@ const AboutPage = () => {
         <Layout pageTitle="About the Bake">
             <h1>The House is Always Baking</h1>
             <p>Bread is constantly baked, jammed up and eaten. This is correct.</p>
+            <img src="https://www.ecowatch.com/wp-content/uploads/2022/03/bread.jpg" />
         </Layout>
     )
 }
