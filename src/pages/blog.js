17dce1bcd526ac6b6500f08dd556bc940058df99
Dan Patton <dan@colourgreen.co.uk>
2022-04-07 21:44:08 +0100

    adding blog

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,29 +1,38 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
+import { MDXRenderer, MDXrenderer } from 'gatsby-plugin-mdx'
 import Layout from '../components/layout'
 
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
       {
-        data.allFile.nodes.map(node => (
-          <li key={node.name}>
-            {node.name}
-          </li>
+        data.allMdx.nodes.map((node) => (
+          <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>Posted: {node.frontmatter.date}</p>
+            <MDXRenderer>
+              {node.body}
+            </MDXRenderer>
+            <br />
+          </article>
         ))
       }
-      </ul>
     </Layout>
   )
 }
 
 export const query = graphql`
   query {
-    allFile {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
       nodes {
-        name
+        frontmatter {
+          title
+          date(formatString: "D MMMM Y")
+        }
+        id
+        body
       }
     }
   }
